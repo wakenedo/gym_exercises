86cc44c51f42d52fb64b1a73ce14710791573af2
wakenedo <a_nuner@outlook.com>
2022-07-11 09:42:15 -0300

    File & Folder Structure
    Setting up the File & Folder Structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom'
+import { Box } from '@mui/material'
+
 import './App.css'
+
+//Components
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
+
+//Pages
+import ExerciseDetail from './pages/ExerciseDetail';
+import Home from './pages/Home';
+
 
 function App() {
   return (
-    <div className="App">
-     <h1>This is Fitness App, Rapid API and Material UI</h1>
-    </div>
+    <Box width='400px' sx={{width: {xl: '1488px'}}} m='auto'>
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/exercise/:id' element={<ExerciseDetail />} />
+
+      </Routes>
+      <Footer />
+    </Box>
   );
 }
 
