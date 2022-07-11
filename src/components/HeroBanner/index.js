673c85a400227070c95ed27e461fb7f903673eaa
wakenedo <a_nuner@outlook.com>
2022-07-11 16:48:27 -0300

    HeroBanner Bug Fix / SearchExercises - Part 1

--- src/components/HeroBanner/index.js
+++ src/components/HeroBanner/index.js
@@ -1,64 +1,67 @@
 import React from 'react'
-import { Box, Stack, Typography, Button } from '@mui/material'
+import { Box, Typography, Button } from '@mui/material'
 import HeroBannerImage from "../../assets/images/banner.png"
 
 const HeroBanner = () => {
     return (
         <Box sx={{
-            mt: {lg: '212px', xs: '70px'},
-            ml: {sm: '50px'}
+            mt: { lg: '212px', xs: '70px' },
+            ml: { sm: '50px' }
         }}
-        position='relative'
-        p='20px'
+            position='relative'
+            p='20px'
         >
-            <Typography 
-            color='#FF2625'
-            fontWeight='600'
-            fontSize='26px'
+            <Typography
+                color='#FF2625'
+                fontWeight='600'
+                fontSize='26px'
             >
                 Fitness Club
             </Typography>
-            <Typography 
-            fontWeight='700' 
-            sx={{
-                fontSize: {lg: '44px', xs: '40px'}
-            }}
-            mb='23px'
-            mt='30px'
+            <Typography
+                fontWeight='700'
+                sx={{
+                    fontSize: { lg: '44px', xs: '40px' }
+                }}
+                mb='23px'
+                mt='30px'
             >
                 Sweat, Smile. <br /> {'&'} Repeat
             </Typography>
             <Typography
-            fontSize='22px'
-            lineHeight='35px'
-            mb='23px'
+                fontSize='22px'
+                lineHeight='35px'
+                mb='23px'
             >
                 Check out the most effective exercises
             </Typography>
             <Button
-            href='#exercises'
-            variant='contained'
-            color='error'
-            sx={{
-                backgroundColor: '#FF2625', 
-                padding: '10px',
-            }}
+                href='#exercises'
+                variant='contained'
+                color='error'
+                sx={{
+                    backgroundColor: '#FF2625',
+                    padding: '10px',
+                }}
             >
                 Explore exercises
             </Button>
             <Typography
-            fontWeight='600'
-            color='#FF2625'
-            sx={{
-                opacity: 0.1,
-                display: { lg: 'block', xs: 'none'},
+                fontWeight='600'
+                color='#FF2625'
+                fontSize='200px'
+                sx={{
+                    opacity: 0.15,
+                    display: { lg: 'block', xs: 'none' },
 
-            }}
-            >EXERCISE
+                }}
+            >
+                EXERCISE
             </Typography>
-            <img src={HeroBannerImage} 
-            alt='banner'
-            className='hero-banner-img'
+            <img src={HeroBannerImage}
+                alt='banner'
+                className='hero-banner-img'
+                
             />
         </Box>
     )
--- src/components/SearchExercises/index.js
+++ src/components/SearchExercises/index.js
@@ -1,8 +1,88 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Box, Button, Stack, TextField, Typography } from '@mui/material'
 
 const SearchExercises = () => {
   return (
-    <div>SearchExercises</div>
+    <Stack
+      alignItems='center'
+      mt='37px'
+      justifyContent='center'
+      p='20px'
+    >
+      <Typography
+        sx={{
+          fontSize: { lg: '44px', xs: '30px' }
+        }}
+        fontWeight='700'
+        mb='50px'
+        textAlign='center'
+      >
+        Awesome Exercises You <br />
+        Should Know
+      </Typography>
+      <Box
+        position='relative'
+        mb='72px'>
+        <TextField
+          sx={{
+            input: {
+              fontWeight: '700',
+              border: 'none',
+              borderRadius: '4px',
+            },
+            width: {
+              lg: '1170px',
+              xs: '350px'
+            },
+            backgroundColor: '#fffff',
+            borderRadius: '40px'
+          }}
+          height='76px'
+          value=''
+          onChange={(e) => { }}
+          placeholder='Search Exercises'
+          type='text'
+        />
+        <Button
+          sx={{
+            backgroundColor: '#FF2625',
+            color: 'white',
+            borderRadius: '4px',
+            textTransform: 'none',
+            height: {
+              xl:'56px',
+              xs: '35px'
+            },
+            marginLeft : {
+              xl: '0',
+              xs: '125px',
+            },
+            marginTop :{
+              xl: '0',
+              xs: '15px',
+
+            },
+            position: {
+              lg: 'absolute'
+            }, 
+            width:
+            {
+              lg: '175px',
+              xs: '100px'
+            },
+            fontSize: {
+              lg:'20px',
+              xs: '14px'
+            }
+          }}
+          variant='contained'
+          color='error'
+
+        >
+          Search
+        </Button>
+      </Box>
+    </Stack>
   )
 }
 
