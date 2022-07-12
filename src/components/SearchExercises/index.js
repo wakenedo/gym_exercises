d65822dbca12480a70024f3a94085735c847cc72
wakenedo <a_nuner@outlook.com>
2022-07-12 16:59:44 -0300

    HorizontalScrollbar Component
    Implemented a Scrollbar with the categories of the muscles that the api provide, for easy user interface adaptation.

--- src/components/SearchExercises/index.js
+++ src/components/SearchExercises/index.js
@@ -2,18 +2,43 @@ import React, { useEffect, useState } from 'react'
 import { Box, Button, Stack, TextField, Typography } from '@mui/material'
 
 //Utils
-import { fetchData, exercisesOptions } from '../../utils/fetchData'
+import { fetchData, exerciseOptions } from '../../utils/fetchData'
 
-const SearchExercises = () => {
+//Components
+import HorizontalScrollbar from '../HorizontalScrollbar'
+
+const SearchExercises = ({ setExercise, bodyPart, setBodyPart }) => {
   const [search, setSearch] = useState('')
+  const [exercises, setExercises] = useState([])
+  const [bodyParts, setBodyParts] = useState([])
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const bodyPartsData = await fetchData(
+        'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
+        exerciseOptions
+      )
+      setBodyParts(['all', ...bodyPartsData])
+    }
+
+    fetchExercisesData();
+  }, [])
 
   const handleSearch = async () => {
     if (search) {
       const exercisesData = await fetchData(
         'https://exercisedb.p.rapidapi.com/exercises',
-        exercisesOptions
+        exerciseOptions
       );
-      console.log(exercisesData)
+      const searchedExercises = exercisesData.filter(
+        (exercise) =>
+          exercise.name.toLowerCase().includes(search)
+          || exercise.target.toLowerCase().includes(search)
+          || exercise.equipment.toLowerCase().includes(search)
+          || exercise.bodyPart.toLowerCase().includes(search)
+      )
+      setSearch('');
+      setExercises(searchedExercises)
     }
   }
 
@@ -101,6 +126,21 @@ const SearchExercises = () => {
         >
           Search
         </Button>
+      </Box>
+      <Box
+
+        sx={{
+          position: 'relative',
+          width: '100%',
+          p: '20px',
+        }}
+      >
+        <HorizontalScrollbar
+          data={bodyParts}
+          bodyPart={bodyPart}
+          setBodyPart={setBodyPart}
+
+        />
       </Box>
     </Stack>
   )
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -7,11 +7,23 @@ import SearchExercises from '../../components/SearchExercises'
 import Exercises from '../../components/Exercises'
 
 const Home = () => {
+    const [exercise, setExercise] = useState([])
+    const [bodyPart, setBodyPart] = useState('all')
+
+
     return (
         <Box>
             <HeroBanner />
-            <SearchExercises />
-            <Exercises />
+            <SearchExercises 
+            setExercise={setExercise}
+            bodyPart={bodyPart}
+            setBodyPart={setBodyPart}
+            />
+            <Exercises 
+            setExercise={setExercise}
+            bodyPart={bodyPart}
+            setBodyPart={setBodyPart}
+            />
         </Box>
     )
 }
