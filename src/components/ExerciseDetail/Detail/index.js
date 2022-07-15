ed38aa429f7d6729709d1f46a36e59cadb37c674
wakenedo <a_nuner@outlook.com>
2022-07-15 20:08:03 -0300

    ExerciseDetail Component fix
    The requests were being made with some errors related to the url. Now is properly setup. Displaying the gif that is indexed with each exercise in the Db.

--- src/components/ExerciseDetail/Detail/index.js
+++ src/components/ExerciseDetail/Detail/index.js
@@ -1,9 +1,44 @@
 import React from 'react'
+import { Stack, Typography, Button } from '@mui/material'
 
-const Detail = () => {
-  return (
-    <div>Detail</div>
-  )
+import BodyPartImage from '../../../assets/icons/body-part.png'
+import TargetImage from '../../../assets/icons/target.png'
+import EquipmentImage from '../../../assets/icons/equipment.png'
+
+const Detail = ({ exerciseDetail }) => {
+    const {
+        bodyPart,
+        gifUrl,
+        name,
+        target,
+        equipment
+    } = exerciseDetail
+
+    return (
+        <Stack
+            gap='60px'
+            sx={{
+                p: '20px',
+                alignItems: 'center',
+                flexDirection: {
+                    lg: 'row',
+                }
+            }}
+        >
+            <Stack sx={{
+                gap: {
+                    lg: '35px',
+                    xs: '20px'
+                }
+            }}>
+                <Typography>
+                    {name}
+                </Typography>
+            </Stack>
+            <img src={gifUrl} alt={name} loading='lazy' className='detail-image' />
+
+        </Stack>
+    )
 }
 
 export default Detail
--- src/pages/ExerciseDetail/index.js
+++ src/pages/ExerciseDetail/index.js
@@ -1,8 +1,39 @@
 import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { Box } from '@mui/material'
+
+
+//Utils
+import { exerciseOptions, fetchData } from '../../utils/fetchData'
+
+//Components
+import Detail from '../../components/ExerciseDetail/Detail'
+import ExerciseVideos from '../../components/ExerciseDetail/ExerciseVideos'
+import SimilarExercises from '../../components/ExerciseDetail/SimilarExercises'
+
 
 const ExerciseDetail = () => {
+  const [exerciseDetail, setExerciseDetail] = useState({});
+  const { id } = useParams()
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const exerciseDbUrl = 'https://exercisedb.p.rapidapi.com'
+      const youtubeSearchUrl = 'https://youtube-search-and-download.p.rapidapi.com'
+
+      const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/exercise/${id}`, exerciseOptions)
+      setExerciseDetail(exerciseDetailData)
+    }
+
+    fetchExercisesData()
+  }, [id])
+
   return (
-    <div>ExerciseDetail</div>
+    <Box>
+      <Detail exerciseDetail={exerciseDetail}/>
+      <ExerciseVideos />
+      <SimilarExercises />
+    </Box>
   )
 }
 
